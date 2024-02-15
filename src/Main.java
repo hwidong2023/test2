7ed77c21273f35c567448e6301580835a7cfaf59
hwidong2023 <hwidong2023@gmail.com>
2024-02-15 09:23:58 +0900

    20240215

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,12 @@ public class Main {
 		System.out.println("프로그램 시작");
 		Scanner sc = new Scanner(System.in);
 		ArrayList<Article> post = new ArrayList<>();
-		int cnt = 1;
+		LocalDateTime now = LocalDateTime.now();
+		String formatedNow = now.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초"));
+		for(int i = 0; i< 3; i++) {
+			post.add(new Article(i+1, "temp",formatedNow, i+1 +" temp"));
+		}
+		int cnt = post.size()+1;
 		while(true) {
 			String test = sc.nextLine();
 			if (test.equals("article list")){
@@ -27,8 +32,8 @@ public class Main {
 				}
 			}
 			else if (test.equals("article write")){
-				LocalDateTime now = LocalDateTime.now();
-				String formatedNow = now.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초"));
+				now = LocalDateTime.now();
+				formatedNow = now.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초"));
 				System.out.print("제목 : ");
 				String tempTitle = sc.nextLine();
 				System.out.print("내용 : ");
@@ -58,6 +63,8 @@ public class Main {
 						System.out.println(num + "번째글 제목 : " + temp.title);
 						System.out.println(num + "번째글 내용 : " + temp.content);
 						System.out.println(num + "번째글 시간 : " + temp.date);
+						post.get(i).views++;
+						System.out.println(num + "번째글 조회수 : " + post.get(i).views);
 						b = true;
 						break;
 					}
@@ -90,6 +97,38 @@ public class Main {
 					System.out.println("게시글이 없습니다.");
 				}
 			}
+			else if (test.startsWith("article modify ") ) {
+				int num;
+				try {
+					num = Integer.parseInt(test.split(" ")[2]);
+				}
+				catch(Exception e) {
+					System.out.println("숫자를 입력하시오");
+					continue;
+				}
+				Article temp = null;
+				int tempNum=-1;
+				for(int i = 0; i< post.size(); i++) {
+					if(post.get(i).id == num){
+						temp = post.get(i);
+						post.remove(i);
+						tempNum = i;
+						break;
+					}
+				}
+				if (tempNum == -1) {
+					System.out.println("게시글이 없습니다.");
+				}
+				else {
+					System.out.print("제목 : ");
+					String tempTitle = sc.nextLine();
+					System.out.print("내용 : ");
+					String tempContent = sc.nextLine();
+					temp.title=tempTitle;
+					temp.content=tempContent;
+					post.add(tempNum,temp);
+				}
+			}
 			else {
 				System.out.println("존재하지 않는 명령어입니다.");
 			}
@@ -104,6 +143,7 @@ class Article {
 	public String title;
 	public String content;
 	public String date;
+	public int views = 0;
 
 	public Article(int id, String title, String date, String content) {
 		this.id =id;
